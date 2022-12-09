fe2819c089c908c53e34127dc628fbeb580806e9
ryfazrin <>
2022-12-09 18:58:47 +0700

    added: requestListener

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Halo, kita akan belajar membuat server');
+const http = require('http');
+
+/**
+ * Logika untuk menangani dan menanggapi request dituliskan pada fungsi ini
+ * 
+ * @param request: objek yang berisikan informasi terkait permintaan
+ * @param response: objek yang digunakan untuk menanggapi permintaan
+ */
+const requestListener = (request, response) => {
+    
+};
+ 
+const server = http.createServer(requestListener);
