d0c29ebfa55445f78d77e4914888dc01f8c1c5d5
ryfazrin <>
2022-12-09 19:46:34 +0700

    added: route and method

--- server.js
+++ server.js
@@ -13,11 +13,29 @@ const requestListener = (request, response) => {
   const { method, url } = request;
 
   if (url === '/') {
-    // TODO 2: logika respons bila url bernilai '/'
+    if(method === 'GET') {
+      response.end('<h1>Ini adalah homepage</h1>');
+    } else {
+      response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+    }
   } else if (url === '/about') {
-    // TODO 3: logika respons bila url bernilai '/about'
+      if (method === 'GET') {
+        response.end('<h1>Halo! Ini adalah halaman about</h1>')
+      } else if (method === 'POST') {
+        let body = [];
+
+        request.on('data', (chunk) => {
+          body.push(chunk);
+        });
+
+        request.on('end', () => {
+          body = Buffer.concat(body).toString();
+          const {name} = JSON.parse(body);
+          response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+        });
+      }
   } else {
-    // TODO 1: logika respons bila url bukan '/' atau '/about'
+    response.end('<h1>Halaman tidak ditemukan!</h1>');
   }
 
   // if (method === 'GET') {
